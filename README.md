fa6498530945e07e0dc225926d54f45a069a1dd0
chirag-ag2 <60141290+chirag-ag2@users.noreply.github.com>
2020-06-24 00:34:46 +0530

    added a fileadd.py to add addition feature to our project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Just a Demo Repository
+
+This file is documentation
+
+`add.py` is simple addition program in python3
---
+++ add.py
@@ -0,0 +1,7 @@
+""" Simple Addition program"""
+
+x=int(input("Enter x: "))
+
+y=int(input("Enter y: "))
+
+print(f"{x} + {y} = {x+y}")
